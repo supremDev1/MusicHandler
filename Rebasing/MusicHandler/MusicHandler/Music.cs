129548e9c606daf9a8e83995d9aee2e9f95c91c6
supremeDev1 <supremeDev1@outlook.fr>
2023-12-25 03:32:45 +0100

    add music class

---
+++ Rebasing/MusicHandler/MusicHandler/Music.cs
@@ -0,0 +1,10 @@
+using System;
+public class Music
+{
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public Music()
+    {
+    }
+}
